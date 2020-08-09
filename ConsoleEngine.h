362f006fc0d062be760798d1f331840661a4388a
VoidSpren <58563231+VoidSpren@users.noreply.github.com>
2020-08-09 03:51:13 -0500

    adds Console3DGraphics class

--- ConsoleEngine.h
+++ ConsoleEngine.h
@@ -1,4 +1,10 @@
 #pragma once
+
+#define _2D_ENGINE
+#ifdef _3D_ENGINE
+#undef _2D_ENGINE
+#endif
+
 #include <Windows.h>
 #include <vector>
 #include <thread>
@@ -14,19 +20,41 @@ struct Vec2{
 
 	Vec2() : x(0), y(0) {}
 	Vec2(T x, T y) : x(x), y(y) {}
-	Vec2(const Vec2<T>& other) : x(other.x), y(other.y) {}
+	Vec2(const Vec2& other) : x(other.x), y(other.y) {}
+
+	static T dotProd(Vec2& a, Vec2& b) { return a.x * b.x + a.y * b.y; }
+	static Vec2 normal(Vec2& in) {
+		float l = sqrtf(in.x * in.x + in.y * in.y);
+		return { in.x / l, in.y / l };
+	}
+
+	void normalize() {
+		float l = sqrtf(x * x + y * y);
+		x / l; y / l;
+	}
+
+	Vec2 operator + (const Vec2& o) { return { x + o.x, y + o.y }; }
+	Vec2 operator - (const Vec2& o) { return { x - o.x, y - o.y }; }
+	Vec2 operator * (const Vec2& o) { return { x * o.x, y * o.y }; }
+	Vec2 operator * (const T s) { return { x * s, y * s }; }
+	Vec2 operator += (const Vec2& o) { x += o.x; y += o.y; return *this; }
+	Vec2 operator -= (const Vec2& o) { x -= o.x; y -= o.y; return *this; }
+	Vec2 operator *= (const Vec2& o) { x *= o.x; y *= o.y; return *this; }
+	Vec2 operator *= (const T& s) { x *= s; y *= s; return *this; }
+
 };
+typedef Vec2<int> Vec2i;
+typedef Vec2<float> Vec2f;
+typedef Vec2<double> Vec2d;
 
 /* simple vector3 struct */
 template<typename T>
 struct Vec3 {
-	T x;
-	T y;
-	T z;
+	T x, y, z;
 
 	Vec3() : x(0), y(0), z(0) {}
 	Vec3(T x, T y, T z) : x(x), y(y), z(z) {}
-	Vec3(const Vec3<T>& other) : x(other.x), y(other.y), z (other.z) {}
+	Vec3(const Vec3& other) : x(other.x), y(other.y), z (other.z) {}
 
 	T vecMin() {
 		if (x <= y && x <= z) return x;
@@ -39,11 +67,114 @@ struct Vec3 {
 		if (z >= x && z >= y) return z;
 	}
 
-	static Vec3 cross(Vec3& a, Vec3& b) {
-		return {a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x };
+	static T dotProd(Vec3& a, Vec3& b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+	static Vec3 cross(Vec3& a, Vec3& b) { return {a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x }; }
+	static Vec3 normal(Vec3& in) {
+		float l = sqrtf(in.x * in.x + in.y * in.y + in.z * in.z);
+		return { in.x / l, in.y / l,in.z / l };
 	}
 
+	void normalize() {
+		float l = sqrtf(x * x + y * y + z * z);
+		x / l; y / l; z / l;
+	}
+
+	Vec3 operator + (const Vec3& o) { return { x + o.x, y + o.y, z + o.z }; }
+	Vec3 operator - (const Vec3& o) { return { x - o.x, y - o.y, z - o.z }; }
+	Vec3 operator * (const Vec3& o) { return { x * o.x, y * o.y, z * o.z }; }
+	Vec3 operator * (const T s) { return { x * s, y * s, z * s }; }
+	Vec3 operator += (const Vec3& o) { x += o.x; y += o.y; z += o.z; return *this; }
+	Vec3 operator -= (const Vec3& o) { x -= o.x; y -= o.y; z -= o.z; return *this; }
+	Vec3 operator *= (const Vec3& o) { x *= o.x; y *= o.y; z *= o.z; return *this; }
+	Vec3 operator *= (const T& s) { x *= s; y *= s; z *= s; return *this; }
+
 };
+typedef Vec3<int> Vec3i;
+typedef Vec3<float> Vec3f;
+typedef Vec3<double> Vec3d;
+
+/*simple vector4 struct*/
+template<typename T>
+struct Vec4 {
+	T x, y, z, w;
+
+	Vec4() : x(0), y(0), z(0), w((T)1) {}
+	Vec4(T x, T y, T z) : x(x), y(y), z(z), w((T)1) {}
+	Vec4(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) {}
+	Vec4(const Vec4& other) : x(other.x), y(other.y), z(other.z), w(other.w) {}
+
+	/*the w component will be ignored in all vec to vec operations, it will only be used in vec to mat operations*/
+
+	static T dotProd(Vec4& a, Vec4& b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+	static Vec4 cross(Vec4& a, Vec4& b) { return { a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x }; }
+	static Vec4 unit(Vec4& in) {
+		float l = sqrtf(in.x * in.x + in.y * in.y + in.z * in.z);
+		return { in.x / l, in.y / l,in.z / l };
+	}
+
+	void toUnit() {
+		float l = sqrtf(x * x + y * y + z * z);
+		x /= l; y /= l; z /= l;
+	}
+
+	Vec4 operator + (const Vec4& o) { return { x + o.x, y + o.y, z + o.z }; }
+	Vec4 operator - (const Vec4& o) { return { x - o.x, y - o.y, z - o.z }; }
+	Vec4 operator * (const Vec4& o) { return { x * o.x, y * o.y, z * o.z }; }
+	Vec4 operator * (const T s) { return { x * s, y * s, z * s }; }
+	Vec4 operator += (const Vec4& o) { x += o.x; y += o.y; z += o.z; return *this; }
+	Vec4 operator -= (const Vec4& o) { x -= o.x; y -= o.y; z -= o.z; return *this; }
+	Vec4 operator *= (const Vec4& o) { x *= o.x; y *= o.y; z *= o.z; return *this; }
+	Vec4 operator *= (const T& s) { x *= s; y *= s; z *= s; return *this; }
+};
+typedef Vec4<int> Vec4i;
+typedef Vec4<float> Vec4f;
+typedef Vec4<double> Vec4d;
+
+template<typename T>
+struct Mat4 {
+	T m[4][4] = { 0 };
+
+	Mat4 operator * (const Mat4& a) {
+		Mat4 c;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				c.m[i][j] = m[i][0] * a.m[0][j] + m[i][1] * a.m[1][j] + m[i][2] * a.m[2][j] + m[i][3] * a.m[3][j];
+			}
+		}
+		return c;
+	}
+	T* operator [] (const int pos) { return m[pos]; }
+	Vec4<T> operator * (const Vec4<T>& vec) {
+		return{
+			vec.x * m[0][0] + vec.y * m[1][0] + vec.z * m[2][0] + vec.w * m[3][0],
+			vec.x * m[0][1] + vec.y * m[1][1] + vec.z * m[2][1] + vec.w * m[3][1],
+			vec.x * m[0][2] + vec.y * m[1][2] + vec.z * m[2][2] + vec.w * m[3][2],
+			vec.x * m[0][3] + vec.y * m[1][3] + vec.z * m[2][3] + vec.w * m[3][3]
+		};
+	}
+	//Vec4<T> * operator (const Vec4<T>& vec) {
+	//	return{
+	//		vec.x * m[0][0] + vec.y * m[1][0] + vec.z * m[2][0] + vec.w * m[3][0],
+	//		vec.x * m[0][1] + vec.y * m[1][1] + vec.z * m[2][1] + vec.w * m[3][1],
+	//		vec.x * m[0][2] + vec.y * m[1][2] + vec.z * m[2][2] + vec.w * m[3][2],
+	//		vec.x * m[0][3] + vec.y * m[1][3] + vec.z * m[2][3] + vec.w * m[3][3]
+	//	};
+	//}
+};
+typedef Mat4<int> Mat4i;
+typedef Mat4<float> Mat4f;
+typedef Mat4<double> Mat4d;
+
+template<typename T>
+Vec4<T> operator * (const Vec4<T>& vec, Mat4<T>& mat) {
+	return{
+		vec.x * mat.m[0][0] + vec.y * mat.m[1][0] + vec.z * mat.m[2][0] + vec.w * mat.m[3][0],
+		vec.x * mat.m[0][1] + vec.y * mat.m[1][1] + vec.z * mat.m[2][1] + vec.w * mat.m[3][1],
+		vec.x * mat.m[0][2] + vec.y * mat.m[1][2] + vec.z * mat.m[2][2] + vec.w * mat.m[3][2],
+		vec.x * mat.m[0][3] + vec.y * mat.m[1][3] + vec.z * mat.m[2][3] + vec.w * mat.m[3][3]
+	};
+}
+
 
 /* class encapsuling console drawing functionality */
 class ConsoleGraphics {
@@ -67,7 +198,7 @@ protected:
 	
 	typedef enum : uint8_t {
 		BLACK, DARK_BLUE = 0x11, DARK_GREEN = 0x22, DARK_CYAN = 0x33,
-		DARK_RED = 0x44, DARK_MAGENTA = 0x55, DARK_YELLOW = 0x66, LIGTH_GREY = 0x77,
+		DARK_RED = 0x44, DARK_MAGENTA = 0x55, DARK_YELLOW = 0x66, LIGHT_GREY = 0x77,
 		GREY = 0x88, BLUE = 0x99, GREEN = 0xAA, CYAN = 0xBB,
 		RED = 0xCC, MAGENTA = 0xDD, YELLOW = 0xEE, WHITE = 0xFF
 	} Color;
@@ -154,6 +285,7 @@ protected:
 		}
 		return 1;
 	}
+
 	bool blendColor(Color main, Color second, uint8_t blend = 0) {
 		if (blend > 3) return 0;
 		switch (blend)
@@ -176,18 +308,20 @@ protected:
 
 	bool greyScale(uint8_t brightness = 11) {
 		if (brightness < 4) blendColor(BLACK, GREY, brightness);
-		else if (brightness < 8) blendColor(GREY, LIGTH_GREY, brightness - 4);
-		else if (brightness < 12) blendColor(LIGTH_GREY, WHITE, brightness - 8);
+		else if (brightness < 8) blendColor(GREY, LIGHT_GREY, brightness - 4);
+		else if (brightness < 12) blendColor(LIGHT_GREY, WHITE, brightness - 8);
 		else return 0;
 		return 1;
 	}
-	/* //still in testing
-	bool brigthColor(Color c, uint8_t brightness = 6) {
-		if (brightness < 7) setColor(c, brightness);
-		else if (brightness < 11) blendColor(c, WHITE, brightness - 7);
+
+	bool brightColor(Color c, uint8_t brightness = 8) {
+		if (brightness < 6) setColor(c, brightness);
+		else if (brightness < 9) {
+			blendColor(c, WHITE, brightness - 6);
+		}
 		else return 0;
 		return 1;
-	}*/
+	}
 
 	/*cleans screenBuffer*/
 	void clear() {
@@ -284,23 +418,23 @@ protected:
 	}
 
 	/*writes a triangle just as the triangle function, and fill it*/
-	void fillTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
-		Vec3<int> xComp(x1, x2, x3);
-		Vec3<int> yComp(y1, y2, y3);
+	virtual void fillTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+		Vec3i xComp(x1, x2, x3);
+		Vec3i yComp(y1, y2, y3);
 
-		Vec2<int> bbmin(xComp.vecMin(), yComp.vecMin());
-		Vec2<int> bbmax(xComp.vecMax(), yComp.vecMax());
+		Vec2i bbmin(xComp.vecMin(), yComp.vecMin());
+		Vec2i bbmax(xComp.vecMax(), yComp.vecMax());
 
-		Vec3<float> a(xComp.y - xComp.x, xComp.z - xComp.x, 0);
-		Vec3<float> b(yComp.y - yComp.x, yComp.z - yComp.x, 0);
+		Vec3f a(xComp.y - xComp.x, xComp.z - xComp.x, 0);
+		Vec3f b(yComp.y - yComp.x, yComp.z - yComp.x, 0);
 
-		Vec3<float> barycentric;
+		Vec3f barycentric;
 
 		for (int x = bbmin.x; x <= bbmax.x; x++) {
 			for (int y = bbmin.y; y <= bbmax.y; y++) {
 				a.z = xComp.x - x;
 				b.z = yComp.x - y;
-				Vec3<float> u = Vec3<float>::cross( a, b );
+				Vec3f u = Vec3f::cross( a, b );
 
 				if (abs(u.z) < 1) continue;
 				barycentric = { 1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z };
@@ -352,7 +486,82 @@ private:
 
 };
 
+
+//still incomplete, use at own discrecion
+class Console3DGraphics : public ConsoleGraphics {
+	float* _zBuffer;
+
+protected:
+
+	Console3DGraphics() {}
+	~Console3DGraphics() { delete[] _zBuffer; }
+
+	bool construct3D() {
+		_zBuffer = new float[width() * height()];
+		for (int i = 0; i < width() * height(); i++) {
+			_zBuffer[i] = 0;
+		}
+		return 1;
+	}
+
+	float get_zBuffer(int x, int y) {
+		if (x < width() && x >= 0 && y < height() && y >= 0) {
+			return _zBuffer[y * width() + x];
+		}
+	}
+	void set_zBuffer(int x, int y, float n) {
+		if (x < width() && x >= 0 && y < height() && y >= 0) {
+			_zBuffer[y * width() + x] = n;
+		}
+	}
+
+	void fillTriangle(Vec4f& p1, Vec4f& p2, Vec4f& p3) {
+		float z;
+
+		Vec3f xComp(p1.x, p2.x, p3.x);
+		Vec3f yComp(p1.y, p2.y, p3.y);
+
+		Vec2i bbmin(xComp.vecMin(), yComp.vecMin());
+		Vec2i bbmax(xComp.vecMax(), yComp.vecMax());
+
+		Vec3f a(xComp.y - xComp.x, xComp.z - xComp.x, 0);
+		Vec3f b(yComp.y - yComp.x, yComp.z - yComp.x, 0);
+
+		Vec3f barycentric;
+
+		for (int x = bbmin.x; x <= bbmax.x; x++) {
+			for (int y = bbmin.y; y <= bbmax.y; y++) {
+				a.z = xComp.x - x;
+				b.z = yComp.x - y;
+				Vec3f u = Vec3f::cross(a, b);
+
+				if (abs(u.z) < 1) continue;
+				barycentric = { 1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z };
+
+				if (barycentric.x >= 0 && barycentric.y >= 0 && barycentric.z >= 0) {
+					z = 0;
+					z += p1.z * barycentric.x;
+					z += p2.z * barycentric.y;
+					z += p3.z * barycentric.z;
+
+					if(get_zBuffer(x, y) < z){
+						set_zBuffer(x, y, z);
+						point(x, y);
+					}
+				}
+			}
+		}
+	}
+};
+
+
+//to use 3D options define _3D_ENGINE before including the header
+#ifdef _2D_ENGINE
 class ConsoleEngine: public ConsoleGraphics {
+#endif
+#ifdef _3D_ENGINE
+class ConsoleEngine : public Console3DGraphics {
+#endif
 	bool keyState[254] = { 0 };
 protected:
 	typedef enum : uint8_t {
@@ -416,8 +625,11 @@ protected:
 
 public:
 	/*starts the engine loop if the renderer is properly set*/
-	bool start() {
-		if (this->set()) {
+	 virtual bool start() {
+		if (set()) {
+#ifdef _3D_ENGINE
+			construct3D();
+#endif
 			std::thread loop(&ConsoleEngine::engineLoop, this);
 			loop.join();
 		}
@@ -440,7 +652,7 @@ private:
 
 			update(fElapsedTime);
 
-			WriteConsoleOutput(hConsole, screenBuffer, bufferSize, { 0,0 }, &windowRect);
+			write();
 		}
 	}
 };
